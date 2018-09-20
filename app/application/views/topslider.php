153a9a20209746b8f023db7dff6964aa5817a246
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-09-20 12:54:18 +0530

    d

--- app/application/views/topslider.php
+++ app/application/views/topslider.php
@@ -20,8 +20,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     .slider-container{
       padding: 0;
     }
-    .slider-container left{
-      padding-left: 30px;
+    .slider-container .left{
+      padding-left: 20px;
     }
 </style>
 <div class="owl-carousel owl-theme">
