475b7e5b08c887b955e0d504d844f27737ed4ad9
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-09-20 12:17:29 +0530

    f

--- app/application/views/topslider.php
+++ app/application/views/topslider.php
@@ -17,43 +17,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         padding: 5px 15px;
         border:none;
     }
+    .slider-container{
+      padding: 0;
+    }
 </style>
 <div class="owl-carousel owl-theme">
     <div class="item" style="">
-        <div class="col-md-12" style="background-color: rgba(0,0,0,0.5)">
-            <div class="col-md-4" style="text-align: right;padding: 10px;">
+        <div class="col-md-12 slider-container" style="background-color: rgba(0,0,0,0.5)">
+            <div class="col-md-4 left" style="text-align: right;padding: 10px;">
                 <p style="color:white">Wildlife - cats</p>
                 <h3 style="color:white">Tiger</h3>
                 <p style="color:white">Experience the majestic tigers of India in different settings from North, West, East, South and Central India. As their population increases and wildlife tourism gets better, no better time than now to visit India for the Royal Bengal Tiger!</p>
                 <button class="topslider-button">KNOW MORE</button>
             </div>
-            <div class="col-md-8 topslider-image" style="background-image: url('<?php echo base_url('assets/img/TigerNATX1.jpg'); ?>');">    
+            <div class="col-md-8 topslider-image right" style="background-image: url('<?php echo base_url('assets/img/TigerNATX1.jpg'); ?>');">
 
             </div>
         </div>
     </div>
     <div class="item" style="">
-        <div class="col-md-12" style="background-color: rgba(0,0,0,0.5)">
-            <div class="col-md-4" style="text-align: right;padding: 10px;">
+        <div class="col-md-12 slider-container" style="background-color: rgba(0,0,0,0.5)">
+            <div class="col-md-4 left" style="text-align: right;padding: 10px;">
                 <p style="color:white">Wildlife - cats</p>
                 <h3 style="color:white">Tiger</h3>
                 <p style="color:white">Experience the majestic tigers of India in different settings from North, West, East, South and Central India. As their population increases and wildlife tourism gets better, no better time than now to visit India for the Royal Bengal Tiger!</p>
                 <button class="topslider-button">KNOW MORE</button>
             </div>
-            <div class="col-md-8 topslider-image" style="background-image: url('<?php echo base_url('assets/img/TigerNATX1.jpg'); ?>');">    
+            <div class="col-md-8 topslider-image right" style="background-image: url('<?php echo base_url('assets/img/TigerNATX1.jpg'); ?>');">
 
             </div>
         </div>
-    </div>
-    <div class="item" style="">
-        <div class="col-md-12" style="background-color: rgba(0,0,0,0.5)">
-            <div class="col-md-4" style="text-align: right;padding: 10px;">
+    </div><div class="item" style="">
+        <div class="col-md-12 slider-container" style="background-color: rgba(0,0,0,0.5)">
+            <div class="col-md-4 left" style="text-align: right;padding: 10px;">
                 <p style="color:white">Wildlife - cats</p>
                 <h3 style="color:white">Tiger</h3>
                 <p style="color:white">Experience the majestic tigers of India in different settings from North, West, East, South and Central India. As their population increases and wildlife tourism gets better, no better time than now to visit India for the Royal Bengal Tiger!</p>
                 <button class="topslider-button">KNOW MORE</button>
             </div>
-            <div class="col-md-8 topslider-image" style="background-image: url('<?php echo base_url('assets/img/TigerNATX1.jpg'); ?>');">    
+            <div class="col-md-8 topslider-image right" style="background-image: url('<?php echo base_url('assets/img/TigerNATX1.jpg'); ?>');">
 
             </div>
         </div>
@@ -71,5 +73,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         autoplayHoverPause: false
     });
 </script>
-
-
