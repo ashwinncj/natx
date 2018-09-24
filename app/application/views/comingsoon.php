3f6a46bf58aa832d6f4d8bec6008ee9e20024b8b
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-09-24 12:45:05 +0530

    Coming soon

---
+++ app/application/views/comingsoon.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<div id="page-parent" style="">
+    <div id="page-layout" style="background-image: url('<?php echo base_url('assets/img/NATXLogo.svg'); ?>'); background-position:center;background-size:cover;">
+    </div>
+</div>
+  
