2d8cf367867fe3adee738c8ee95598b7c5708277
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-09-24 12:55:26 +0530

    f

--- app/application/views/comingsoon.php
+++ app/application/views/comingsoon.php
@@ -2,6 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div id="page-parent" style="">
-    <div id="page-layout" style="background-image: url('<?php echo base_url('assets/img/NATXLogo.png'); ?>'); background-position:center;background-size:cover;">
+    <div id="page-layout" style="background-image: url('<?php echo base_url('assets/img/NATXLogo.png'); ?>'); background-position:center;background-size:contain;height: 100vh;">
+      <h3>
+        Coming soon...
+      </h3>
     </div>
 </div>
