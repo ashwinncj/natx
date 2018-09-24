d08f923f32d54b52382128d8475f56a867c3aabc
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-09-24 13:01:22 +0530

    COming soon page reset

--- app/application/views/comingsoon.php
+++ app/application/views/comingsoon.php
@@ -4,8 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto|Roboto+Mono" rel="stylesheet">
 <div id="page-parent" style="">
     <div id="page-layout" style="text-align: center;background-image: url('<?php echo base_url('assets/img/NATXLogo.png'); ?>'); background-position:center;background-size:contain;height: 100vh;background-repeat:no-repeat">
-      <h3 style="font-size:50px;font-family:Raleway;">
+      <p style="font-size:50px;font-family:Raleway;">
         Coming soon...
-      </h3>
+      </p>
     </div>
 </div>
