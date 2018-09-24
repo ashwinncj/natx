73030df8ac7c52770dfed9e993cc83a239c9ac9b
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-09-24 12:50:53 +0530

    d

--- app/application/views/comingsoon.php
+++ app/application/views/comingsoon.php
@@ -2,7 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div id="page-parent" style="">
-    <div id="page-layout" style="background-image: url('<?php echo base_url('assets/img/NATXLogo.svg'); ?>'); background-position:center;background-size:cover;">
+    <div id="page-layout" style="background-image: url('<?php echo base_url('assets/img/NATXLogo.png'); ?>'); background-position:center;background-size:cover;">
     </div>
 </div>
-  
