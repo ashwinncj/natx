bb4e51b15b10713bfdbba924e6f5cf64c5951136
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-10-11 15:33:56 +0530

    Package files

---
+++ app/application/views/packages/dudhwaphotography.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<style>
+    #page-layout{
+        background-color: rgba(255,255,255,0.85);
+    }
+</style>
+<title>NATX - Dudhwa and Bardia</title>
+<div id="page-parent" style="background-image: url('<?php echo base_url('assets/img/packages/wildlife_dudhwaimage.jpg'); ?>'); background-position:center;background-size:cover;"> 
+    <!--<div id="page-parent" style="position:relative">-->
+    <?php //$this->load->view('topslider'); ?>
+    <div id="page-layout" style="padding: 20px">
+        <div style="background-image: url('<?php echo base_url('assets/img/packages/wildlife_dudhwaimage.jpg'); ?>'); height: 300px;background-position:center;background-size:cover;"> 
+        </div>
+        <h1>
+            <!--        //Put heading here-->
+            Dudhwa and Bardia
+        </h1>
+        <div style="padding-left:30px;padding-right: 30px">
+            <!--        //Put description here-->
+            <h3>Day1</h3>
+            <p>Arrival at New Delhi</p>
+            <h3>Day2</h3>
+            <p>Leave to Lucknow by morning flight - Road transfer (5 hours drive) to Dudhwa (variety of Breakfast options on the way or we arrange packed to save time). 
+                Check in at Original home of The Legend Billy Arjan Singh’s home converted in to Resort and run by Mr. Shaminder, writer of Billy Arjan Singh’s Biography (A well furnished resort equipped with 5 rooms, beautiful dining area, and a deck overlooking waterbody and the jungle in the backdrop to give the wildlife feeling while you’re in the resort itself) 
+                Afternoon lunch at the resort. 
+            </p>
+			<h3>Day 3</h3>
+			<p>Morning safari from the main gate of Dudhwa National park followed by breakfast at the resort. After Lunch at 1.30 Pm, We will take you to the Tiger Land, a remarkable forest called Kishanpur Wildlife Sanctuary, a 40 minutes drive in an SUV.  Open Safari jeeps will be waiting for us at the forest gates. You can spend some quality time with majestic Terai tigers (Foothills of Himalayas bordering Nepal). 
+Roof top dinner at 8.00 PM</p>
+        </div>
+    </div>
+</div>
