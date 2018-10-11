94d79297b573889c8966d9ebc616c1e4b9a8637e
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-10-11 16:25:09 +0530

    Update app/application/views/home.php

--- app/application/views/home.php
+++ app/application/views/home.php
@@ -82,86 +82,98 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     }
 </style>
 <title>NATX</title>
-<!-- <div id="page-parent" style="background-image: url('<?php //echo base_url('assets/img/MGL08175DsR9449.jpg');     ?>'); background-position:center;background-size:cover;"> -->
+<!-- <div id="page-parent" style="background-image: url('<?php //echo base_url('assets/img/MGL08175DsR9449.jpg');       ?>'); background-position:center;background-size:cover;"> -->
 <div id="page-parent" style="position:relative">
     <?php $this->load->view('topslider'); ?>
     <div id="page-layout" style="">
         <div class="col-md-4 tile-parent">
-            <div class="rotate tile">
-                <div class="front" style="background-color:rgba(48,50,49,1)">
-                    <p class="title">Nature</p>
-                    <p class="description">
-                        A large country with variety of terrains and landscapes makes India, a jewel for Nature visit! Come on our tours to experience the beauty of the nation!
-                    </p>
-                    <p class="readmore">More</p>
+            <a href="<?php echo base_url('nature'); ?>">
+                <div class="rotate tile">
+                    <div class="front" style="background-color:rgba(48,50,49,1)">
+                        <p class="title">Nature</p>
+                        <p class="description">
+                            A large country with variety of terrains and landscapes makes India, a jewel for Nature visit! Come on our tours to experience the beauty of the nation!
+                        </p>
+                        <p class="readmore">More</p>
+                    </div>
+                    <div class="back" style="background-image: url('<?php echo base_url('assets/img/BTR05171DxMii6350.jpg'); ?>');">
+                    </div>
                 </div>
-                <div class="back" style="background-image: url('<?php echo base_url('assets/img/BTR05171DxMii6350.jpg'); ?>');">
-                </div>
-            </div>
+            </a>
         </div>
         <div class="col-md-4 tile-parent">
-            <div class="rotate tile">
-                <div class="front" style="background-color:rgba(21,37,43,1)">
-                    <p class="title">Wildlife</p>
-                    <p class="description">
-                        With over a decade of experience traveling the wild in India, the team has come up with packages for a comprehensive but entertaining list of wildlife tours.
-                    </p>
-                    <p class="readmore">More</p>
+            <a href="<?php echo base_url('wildlife'); ?>">
+                <div class="rotate tile">
+                    <div class="front" style="background-color:rgba(21,37,43,1)">
+                        <p class="title">Wildlife</p>
+                        <p class="description">
+                            With over a decade of experience traveling the wild in India, the team has come up with packages for a comprehensive but entertaining list of wildlife tours.
+                        </p>
+                        <p class="readmore">More</p>
+                    </div>
+                    <div class="back" style="background-image: url('<?php echo base_url('assets/img/KBN01161Dx-0973.jpg'); ?>');">
+                    </div>
                 </div>
-                <div class="back" style="background-image: url('<?php echo base_url('assets/img/KBN01161Dx-0973.jpg'); ?>');">
-                </div>
-            </div>
+            </a>
         </div>
         <div class="col-md-4 tile-parent">
-            <div class="rotate tile">
-                <div class="front" style="background-color:rgba(20,47,48,1)">
-                    <p class="title">Diving</p>
-                    <p class="description">
-                        Ever wondered how it could be, if you could breath underwater and enjoy that world unseen by a normal man! Take up a SCUBA diving course and become a diver. Our professional team help you with a course to suit your needs
-                        Not Just that! You can plan your dive holidays from a list, curated with personal experience, to suit your ability and budget.
-                    </p>
-                    <p class="readmore">More</p>
+            <a href="<?php echo base_url(''); ?>">
+                <div class="rotate tile">
+                    <div class="front" style="background-color:rgba(20,47,48,1)">
+                        <p class="title">Diving</p>
+                        <p class="description">
+                            Ever wondered how it could be, if you could breath underwater and enjoy that world unseen by a normal man! Take up a SCUBA diving course and become a diver. Our professional team help you with a course to suit your needs
+                            Not Just that! You can plan your dive holidays from a list, curated with personal experience, to suit your ability and budget.
+                        </p>
+                        <p class="readmore">More</p>
+                    </div>
+                    <div class="back" style="background-image: url('<?php echo base_url('assets/img/BKN10175DsR2263.jpg'); ?>');">
+                    </div>
                 </div>
-                <div class="back" style="background-image: url('<?php echo base_url('assets/img/BKN10175DsR2263.jpg'); ?>');">
-                </div>
-            </div>
+            </a>
         </div>
         <div class="col-md-4 tile-parent">
-            <div class="rotate tile">
-                <div class="front" style="background-color:rgba(50,78,93,1)">
-                    <p class="title">Shop</p>
-                    <p class="description">
-                        We are pleased to sell Made in India items at world class quality for a variety of needs. Serious experience and thought has gone in designing these items.     
-                    </p>
-                    <p class="readmore">More</p>
+            <a href="<?php echo base_url(''); ?>">
+                <div class="rotate tile">
+                    <div class="front" style="background-color:rgba(50,78,93,1)">
+                        <p class="title">Shop</p>
+                        <p class="description">
+                            We are pleased to sell Made in India items at world class quality for a variety of needs. Serious experience and thought has gone in designing these items.     
+                        </p>
+                        <p class="readmore">More</p>
+                    </div>
+                    <div class="back" style="background-image: url('<?php echo base_url('assets/img/ANP11165DsR2138Pano.jpg'); ?>');">
+                    </div>
                 </div>
-                <div class="back" style="background-image: url('<?php echo base_url('assets/img/ANP11165DsR2138Pano.jpg'); ?>');">
-                </div>
-            </div>
+            </a>
         </div>
         <div class="col-md-4 tile-parent">
-            <div class="rotate tile dark">
-                <div class="front" style="background-color:rgba(229,225,228,1)">
-                    <p class="title">Blog & Gallery</p>
-                    <p class="description">
-                        We share thoughts from our guests and use in our blogs. Our guests can send us their images which once selected, will be posted in our gallery so they can also relate to the shared experience.     
-                    </p>
-                    <p class="readmore">More</p>
+            <a href="<?php echo base_url(''); ?>">
+                <div class="rotate tile dark">
+                    <div class="front" style="background-color:rgba(229,225,228,1)">
+                        <p class="title">Blog & Gallery</p>
+                        <p class="description">
+                            We share thoughts from our guests and use in our blogs. Our guests can send us their images which once selected, will be posted in our gallery so they can also relate to the shared experience.     
+                        </p>
+                        <p class="readmore">More</p>
+                    </div>
+                    <div class="back" style="background-image: url('<?php echo base_url('assets/img/HLK02165DsR3575.jpg'); ?>');">
+                    </div>
                 </div>
-                <div class="back" style="background-image: url('<?php echo base_url('assets/img/HLK02165DsR3575.jpg'); ?>');">
-                </div>
-            </div>
+            </a>
         </div>
         <div class="col-md-4 tile-parent">
-            <div class="rotate tile dark">
-                <div class="front" style="background-color:rgba(166,170,159,1)">
-                    <p class="title">Contact</p>
-                    <p class="description"></p>
-                    <p class="readmore">More</p>
+            <a href="<?php echo base_url(''); ?>">
+                <div class="rotate tile dark">
+                    <div class="front" style="background-color:rgba(166,170,159,1)">
+                        <p class="title">Contact</p>
+                        <p class="description"></p>
+                        <p class="readmore">More</p>
+                    </div>
+                    <div class="back" style="background-image: url('<?php echo base_url('assets/img/LBH10175DsR0249.jpg'); ?>');">
+                    </div>
                 </div>
-                <div class="back" style="background-image: url('<?php echo base_url('assets/img/LBH10175DsR0249.jpg'); ?>');">
-                </div>
-            </div>
+            </a>
         </div>        
     </div>
 </div>
