96d89f00d7c6ea3bb2b6912483e907ef90e93770
HarshithaMavoori <30402591+HarshithaMavoori@users.noreply.github.com>
2018-10-11 16:28:19 +0530

    Update dudhwaphotography.php

--- app/application/views/packages/dudhwaphotography.php
+++ app/application/views/packages/dudhwaphotography.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         background-color: rgba(255,255,255,0.85);
     }
 </style>
-<title>NATX - Dudhwa and Bardia</title>
+<title>NATX - Dudhwa (Photography Only)</title>
 <div id="page-parent" style="background-image: url('<?php echo base_url('assets/img/packages/wildlife_dudhwaimage.jpg'); ?>'); background-position:center;background-size:cover;"> 
     <!--<div id="page-parent" style="position:relative">-->
     <?php //$this->load->view('topslider'); ?>
@@ -15,20 +15,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
         <h1>
             <!--        //Put heading here-->
-            Dudhwa and Bardia
+            Dudhwa (Photography Only)
         </h1>
         <div style="padding-left:30px;padding-right: 30px">
-            <!--        //Put description here-->
-            <h3>Day1</h3>
-            <p>Arrival at New Delhi</p>
-            <h3>Day2</h3>
-            <p>Leave to Lucknow by morning flight - Road transfer (5 hours drive) to Dudhwa (variety of Breakfast options on the way or we arrange packed to save time). 
-                Check in at Original home of The Legend Billy Arjan Singh’s home converted in to Resort and run by Mr. Shaminder, writer of Billy Arjan Singh’s Biography (A well furnished resort equipped with 5 rooms, beautiful dining area, and a deck overlooking waterbody and the jungle in the backdrop to give the wildlife feeling while you’re in the resort itself) 
-                Afternoon lunch at the resort. 
-            </p>
-			<h3>Day 3</h3>
-			<p>Morning safari from the main gate of Dudhwa National park followed by breakfast at the resort. After Lunch at 1.30 Pm, We will take you to the Tiger Land, a remarkable forest called Kishanpur Wildlife Sanctuary, a 40 minutes drive in an SUV.  Open Safari jeeps will be waiting for us at the forest gates. You can spend some quality time with majestic Terai tigers (Foothills of Himalayas bordering Nepal). 
-Roof top dinner at 8.00 PM</p>
+        	<!--        //Put description here-->
+        	<h3>Day 1</h3>
+        	<p>Reach Lucknow by morning flight - Road transfer (5 hours drive) to Dudhwa (variety of Breakfast options on the way or we arrange packed to save time). 
+		<br>
+		Check in at Original home of The Legend Billy Arjan Singh’s home converted in to Resort and run by Mr. Shaminder, writer of Billy Arjan Singh’s Biography (A well furnished resort equipped with 5 rooms, beautiful dining area, and a deck overlooking waterbody and the jungle in the backdrop to give the wildlife feeling while you’re in the resort itself) 
+		<br>
+		Afternoon lunch at the resort.
+		<br><br>
+		Horse Riders !! NOTE - You can even rent an horse and ride in to the buffer areas of the jungle in the evening. Also an option to do birding in and around the Resort. Resort itself is a 40 acre property with it’s own water body. An ideal place for birding as it has many resident and migratory bird species.
+		</p>
+        	<h3>Day 2</h3>
+        	<p> 
+        	</p>
+		<h3>Day 3</h3>
+		<p>Morning safari from the main gate of Dudhwa National park followed by breakfast at the resort. After Lunch at 1.30 Pm, We will take you to the Tiger Land, a remarkable forest called Kishanpur Wildlife Sanctuary, a 40 minutes drive in an SUV.  Open Safari jeeps will be waiting for us at the forest gates. You can spend some quality time with majestic Terai tigers (Foothills of Himalayas bordering Nepal). 
+		<br>
+		Roof top dinner at 8.00 PM
+		</p>
+		<h3>Day 4</h3>
+		<p>We will return to Kishanpur Wildlife sanctuary for the feel of the jungle in the morning with another chance to see and photograph tigers. We will return to resort for breakfast. 
+		<br>
+		Our fourth safari of the trip will be again from the main gate of Dudhwa National park, to get a view the forest in the evening golden light.  Evening refreshments from the deck of resort followed by dinner.
+		</p>
+		<h3>Day 5</h3>
+		<p>Morning safari from the main gate of Dudhwa National park followed by breakfast at the resort. After Lunch at 1.30 PM, Kishanpur Wildlife Sanctuary, for evening safari followed by dinner.
+		</p>
+		<h3>Day 6</h3>
+		<p>Morning safari from the Sathiana gate of Dudhwa National park and an evening visit to local villages and historical monuments if all agree otherwise another safari in main gate of Dudhwa National park.
+		<br><br>
+		We will change point of entry for safari depending on the latest news about sightings to give our guests the maximum opportunity to photograph.
+		</p>
+		<h3>Day 7</h3>
+		<p>We will check out from the hotel after breakfast to head back to Lucknow and catch our flights home with memories to cherish till the next trip.
+		</p>
         </div>
     </div>
 </div>
