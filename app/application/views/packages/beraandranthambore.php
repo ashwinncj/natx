ba91f56953af53c833867a0406000bffd2f987c1
HarshithaMavoori <30402591+HarshithaMavoori@users.noreply.github.com>
2018-10-11 15:46:33 +0530

    Update beraandranthambore.php

--- app/application/views/packages/beraandranthambore.php
+++ app/application/views/packages/beraandranthambore.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         background-color: rgba(255,255,255,0.85);
     }
 </style>
-<title>NATX - Dudhwa and Bardia</title>
+<title>NATX - Bera and Ranthambore</title>
 <div id="page-parent" style="background-image: url('<?php echo base_url('assets/img/packages/wildlife_dudhwaimage.jpg'); ?>'); background-position:center;background-size:cover;"> 
     <!--<div id="page-parent" style="position:relative">-->
     <?php //$this->load->view('topslider'); ?>
@@ -15,20 +15,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
         <h1>
             <!--        //Put heading here-->
-            Dudhwa and Bardia
+            Bera and Ranthambore
         </h1>
         <div style="padding-left:30px;padding-right: 30px">
             <!--        //Put description here-->
             <h3>Day1</h3>
-            <p>Arrival at New Delhi</p>
+            <p>Arrival at New Delhi and overnight stay at Airport hotel</p>
             <h3>Day2</h3>
-            <p>Leave to Lucknow by morning flight - Road transfer (5 hours drive) to Dudhwa (variety of Breakfast options on the way or we arrange packed to save time). 
-                Check in at Original home of The Legend Billy Arjan Singh’s home converted in to Resort and run by Mr. Shaminder, writer of Billy Arjan Singh’s Biography (A well furnished resort equipped with 5 rooms, beautiful dining area, and a deck overlooking waterbody and the jungle in the backdrop to give the wildlife feeling while you’re in the resort itself) 
-                Afternoon lunch at the resort. 
+            <p>Leave to Udaipur by morning flight - Road transfer (3 hours drive) to Bera. Afternoon lunch followed by a evening safari. Stay at Heritage villas, Bera. Dinner and cultural show at night 
             </p>
-			<h3>Day 3</h3>
-			<p>Morning safari from the main gate of Dudhwa National park followed by breakfast at the resort. After Lunch at 1.30 Pm, We will take you to the Tiger Land, a remarkable forest called Kishanpur Wildlife Sanctuary, a 40 minutes drive in an SUV.  Open Safari jeeps will be waiting for us at the forest gates. You can spend some quality time with majestic Terai tigers (Foothills of Himalayas bordering Nepal). 
-Roof top dinner at 8.00 PM</p>
-        </div>
+	    <h3>Day 3</h3>
+	    <p>Early morning Open jeep safari (after tea / coffee) from dawn to 10 AM. Break fast with a brief resting time before afternoon lunch. Such at 1.30 PM followed by afternoon safari in the arid hills of Bera from early evening till dusk. After safari come to resort for freshen up and leave for a theme dinner at JAWAI reservoir. Return to the resort after a beautiful evening at Jawai
+	    </p>
+	    <h3>Day 4</h3>
+	    <p>Morning safari and evening safari asper day 3 (In the rest hours people who are willing can go for a village safari where you can experience the local tradition with an opportunity to interact and take some memorable portraits) Evening a wildlife movie about Bera and surroundings followed by dinner.
+	    </p>
+	    <h3>Day 5</h3>
+	    <p>Morning safari followed by break fast at resort. We will check out at 11 AM and leave to our next destination for bigger cats TIGERS. Reach Ranthambore by late evening. Check in to the resort. Dinner and rest for the day to get ready for a Ride a De Tigers next morning.
+	    </p>
+	    <h3>Day 6</h3>
+	    <p>Tea / coffee at cafeteria followed by morning safari till 11.00 AM. (We can opt for packed breakfast or come back for breakfast). Afternoon safari from early evening to dusk followed by a dinner at 8.00 PM at resort.
+	    </p>
+	    <h3>Day 7</h3>
+	    <p>
+	    </p>
+	    <h3>Day 8</h3>
+	    <p>A full day opportunity to spend time inside the park with a chance to see as many tigers as possible in their natural habitat. This is a Dawn to Dusk safari with breakfast and Lunch served in the vehicle itself. An opportunity available to very view selected tourists every day and booked in advance to accommodate you. We will return to hotel for an early dinner and retire to bed early for the next day.
+	    </p>
+	    <h3>Day 9</h3>
+	    <p>Tea / coffee at cafeteria followed by morning safari till 11.00 AM. (We can opt for packed break fast or come back for break fast). Afternoon safari from early evening to dusk. We will leave to Jaipur by 7 PM. Stay at a good hotel in Jaipur for the night.
+	    </p>
+	    <h3>Day 10</h3>
+	    <p>Early Break fast followed by a local tour and shopping around Jaipur. Catch the evening flight to Delhi for onward journey home. We will plan flight to Delhi to reach by 7.00 PM in Delhi Airport.
+	    </p>
+	</div>
     </div>
 </div>
