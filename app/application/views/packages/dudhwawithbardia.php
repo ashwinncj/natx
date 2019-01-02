cdec676d1fbb198df04f173df178c2917c8383ea
Ashwin Kumar C <ashwinncj@gmail.com>
2019-01-02 13:05:22 +0530

    Update dudhwawithbardia.php

--- app/application/views/packages/dudhwawithbardia.php
+++ app/application/views/packages/dudhwawithbardia.php
@@ -15,61 +15,73 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
         <h1>
             <!--        //Put heading here-->
-            Dudhwa and Bardia
+            Dudhwa (India) with Bardia (Nepal)
         </h1>
         <div style="padding-left:30px;padding-right: 30px">
             <!--        //Put description here-->
             <h3>Day 1</h3>
-            <p>Arrival at New Delhi</p>
+            <p>Arrival at New Delhi and overnight stay at airport hotel</p>
             <h3>Day 2</h3>
             <p>
-                Leave to Lucknow by morning flight - Road transfer (5 hours drive) to Dudhwa (variety of Breakfast options on the way or we arrange packed to save time). 
-                Check in at Original home of The Legend Billy Arjan Singh's home converted in to Resort and run by Mr. Shaminder, writer of Billy Arjan Singh's Biography (A well furnished resort equipped with 5 rooms, beautiful dining area, and a deck overlooking waterbody and the jungle in the backdrop to give the wildlife feeling while you're in the resort itself) 
+                Leave to Lucknow by morning flight - Road transfer (5 hours drive) to Dudhwa (we arrange packed to save time).Leave to Lucknow by morning flight - Road transfer (5 hours drive) to Dudhwa (we arrange packed to save time). 
+                Check in to the Original home of “The Legend”, Billy Arjan Singh’s home, run by Mr. Shaminder, writer of Billy Arjan Singh’s Biography (A well furnished home stay, equipped with 5 rooms, beautiful dining area and a deck overlooking waterbody with the jungle in the backdrop to give the feeling of being in a wilderness place) 
                 Afternoon lunch at the resort. 
                 <br><br>
-                Horse Riders !! NOTE - You can even rent an horse and ride in to the buffer areas of the jungle in the evening. Also an option to do birding in and around the Resort. Resort itself is a 40 acre property with it's own water body. An ideal place for birding as it has many resident and migratory bird species.
+                Horse Riders !! NOTE - You can even rent a horse and ride in to the buffer areas of the jungle in the evening. Also an option to do birding in and around the home stay. Hem stay, itself is a 40 acre property with it’s own water body. An ideal place for birding as it has many resident and migratory bird species.
                 <br><br>
-                A Grand Cocktail dinner hosted by us with Unlimited Local Beer, assortment of local wines and Barbecue
+                A Grand Cocktail dinner hosted by us with unlimited local beer, assortment of local wines and barbecue.
             </p>
             <h3>Day 3</h3>
             <p>
-                Morning safari from the main gate of Dudhwa National park followed by breakfast at the resort. After Lunch at 1.30 Pm, We will take you to the Tiger Land, a remarkable forest called Kishanpur Wildlife Sanctuary, a 40 minutes drive in an SUV.  Open Safari jeeps will be waiting for us at the forest gates. You can spend some quality time with majestic Terai tigers (Foothills of Himalayas bordering Nepal). 
+                Morning safari from the main gate of Dudhwa National Park followed by breakfast at the home stay. After lunch at 1.30 PM, We will take you to the “Tiger Land”, a remarkable forest called Kishenpur Wildlife Sanctuary, a 40 minutes drive in an SUV.  Open safari jeeps will be waiting for us at the forest gates. You can spend some quality time with majestic Terai tigers (Foothills of Himalayas bordering Nepal). 
                 Roof top dinner at 8.00 PM</p>
             <h3>Day 4</h3>
             <p>
-                We will return to Kishanpur Wildlife sanctuary for the feel of the jungle in the morning with another chance to see and photograph tigers. We will return to resort for breakfast. 
+                We will return to Kishenpur Wildlife Sanctuary for the feel of the jungle in the morning with another chance to see and photograph tigers. We will return to resort for breakfast.We will return to Kishenpur Wildlife Sanctuary for the feel of the jungle in the morning with another chance to see and photograph tigers. We will return to resort for breakfast. 
                 <br><br>
-                Our fourth safari of the trip will be again from the main gate of Dudhwa National park, to get a view the forest in the evening golden light.  Evening refreshments from the deck of resort followed by dinner.</p>
+                Our fourth safari of the trip will be again from the main gate of Dudhwa National park, to get a view the forest in the evening golden light.  Evening refreshments from the deck of the home stay followed by dinner.</p>
             <h3>Day 5 </h3>
             <p>
-                We will proceed on another exciting part of the trip !! BARDIA National park, Nepal. Bardia National Park is one of Nepal's best kept secrets. This is the largest national park in the Terai, means Grasslands, in the foothills of Himalayas. This trip of Bardia is bound to stay in your memories as one of the most enduring part of the journey.
+                We will proceed on another exciting part of the trip !! BARDIA National Park, Nepal. Bardia National Park is one of Nepal’s best kept secrets. This is the largest national park in the Terai, means grasslands, in the foothills of Himalayas. This trip of Bardia is bound to stay in your memories as one of the most enduring part of the journey.We will proceed on another exciting part of the trip !! BARDIA National Park, Nepal. Bardia National Park is one of Nepal’s best kept secrets. This is the largest national park in the Terai, means grasslands, in the foothills of Himalayas. This trip of Bardia is bound to stay in your memories as one of the most enduring part of the journey.
                 <br><br>
-                We will leave after breakfast at 8.00 am for a comfortable journey of 3 hours to reach the resort before lunch time. A little extra time may be required at the border for immigration, and vehicle permits.
+                We will leave after breakfast at 8.00 AM for a comfortable journey of 4 hours to reach the resort before lunch time. A little extra time may be required at the border for immigration, and vehicle permits.
                 Depending on the time of arrival and lunch, we will try to accommodate an evening safari in open jeeps. Here we can expect to sight the endangered Royal Bengal tiger, Nepal's famous one-horned rhinoceros, swamp deer, blackbuck, more than 200 species of birds, crocodiles and many other mammals.
             </p>
             <h3>Day 6</h3>
             <p>
-                A day with two safaris in Bardia national park. With a comfortable stay and a chance to taste the local cuisine
+                A full day safari in Bardia national park. With a comfortable stay and a chance to taste the local cuisine.
             </p>
             <h3>Day 7</h3>
             <p>
-                We will be doing our fourth safari here before heading back to Dudhwa by late evening. We will return to the resort for a early dinner to get ready for another exciting day tomorrow.
+                A full day safari by walk in Bardia national park. Stay by nightA full day safari by walk in Bardia national park. Stay by night
             </p>
             <h3>Day 8</h3>
             <p>
-                A day to spend in another hidden jewel of Dudhwa, Katarniaghat Wildlife Sanctuary. One of the best places in the world to see Gharial (Indian Mugger) and Gangetic Dolphins in their natural habitat while on a boat safari. Apart from this we will also have chance to see Tigers, Rhinos, Long billed vultures, Bengal floricans and Swamp deer.
+                We will be back to Dudhwa after breakfast. We will return to the home stay for an early dinner to get ready for another exciting day tomorrow.
                 <br><br>
                 Between the safari we will host a bush lunch with Packed breakfast, tea and snacks.
                 Return to safari for dinner.
                 This will be the last day in the wild. 
             </p>
-            <h3>Day 9</h3>
+			<h3>Day 8</h3>
             <p>
-                Today we will head to another authentic Indian experience, The City of Lucknow know for it's amazing local cuisines, and traditional clothing. You can eat and shop to your heart's desire. To accommodate more time for this, we will leave early (around 8.00 AM) to reach Lucknow for sumptuous lunch with some rest in the afternoon followed by shopping, and local restaurant for dinner.
+                We will be back to Dudhwa after breakfast. We will return to the home stay for an early dinner to get ready for another exciting day tomorrow.
+            </p>
+			<h3>Day 9</h3>
+            <p>
+                A day to spend in another hidden jewel of Dudhwa, Katarniaghat Wildlife Sanctuary. One of the best places in the world to see Gharial, Mugger Indian Crocodile) and Gangetic Dolphins in their natural habitat while on a boat safari. Apart from this we will also have chance to see Tigers, Rhinos, Long billed vultures, Bengal floricans and Swamp deer.
+                <br><br>
+                Between the safari we will host a bush packed breakfast, lunch, tea and snacks.
+                Return to the home stay for dinner.
+                This will be the last day in the wild. 
             </p>
             <h3>Day 10</h3>
             <p>
-                We will check out from the hotel after breakfast to head back home with memories to cherish till the next trip. Flight to Delhi before noon to reach in time for onward journey.
+                Today we will head to another authentic Indian experience, The City of Lucknow know for its amazing local cuisines  and traditional clothing. You can eat and shop to your heart’s desire. To accommodate more time for this, we will leave early (around 8.00 AM) to reach Lucknow for sumptuous lunch with some rest in the afternoon followed by shopping and local restaurant for dinner
+            </p>
+            <h3>Day 11</h3>
+            <p>
+                We will check out from the hotel after breakfast to head back home with memories to cherish till the next trip. Flight to Delhi before noon to reach in time for onward journey.We will check out from the hotel after breakfast to head back home with memories to cherish till the next trip. Flight to Delhi before noon to reach in time for onward journey.
             </p>
         </div>
     </div>
