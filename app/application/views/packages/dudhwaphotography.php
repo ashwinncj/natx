1a993fcf3e0a5535e9cea7ff45816ca2b82aca54
HarshithaMavoori <30402591+HarshithaMavoori@users.noreply.github.com>
2019-01-04 18:20:20 +0530

    Update dudhwaphotography.php

--- app/application/views/packages/dudhwaphotography.php
+++ app/application/views/packages/dudhwaphotography.php
@@ -50,5 +50,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>We will check out from the hotel after breakfast to head back to Lucknow and catch our flights home with memories to cherish till the next trip.
 		</p>
         </div>
+	    <div class="text-center">
+            <a href="<?php echo base_url('wildlife'); ?>">
+                <button class="btn btn-primary">Go Back</button>
+            </a>
+            <a href="<?php echo base_url(); ?>">
+                <button class="btn btn-primary">Home</button>
+            </a>
+        </div>
     </div>
 </div>
