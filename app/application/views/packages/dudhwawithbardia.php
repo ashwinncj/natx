a777586144c6728f25366150d9f45f058a042d99
HarshithaMavoori <30402591+HarshithaMavoori@users.noreply.github.com>
2019-01-04 16:56:19 +0530

    Update dudhwawithbardia.php

--- app/application/views/packages/dudhwawithbardia.php
+++ app/application/views/packages/dudhwawithbardia.php
@@ -7,11 +7,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     }
 </style>
 <title>NATX - Dudhwa and Bardia</title>
-<div id="page-parent" style="background-image: url('<?php echo base_url('assets/img/packages/wildlife_bardia.jpg'); ?>'); background-position:center;background-size:cover;"> 
+<div id="page-parent" style="background-image: url('<?php echo base_url('assets/img/packageheader/header_dudhwa.jpg'); ?>'); background-position:center;background-size:cover;"> 
     <!--<div id="page-parent" style="position:relative">-->
     <?php //$this->load->view('topslider'); ?>
     <div id="page-layout" style="padding: 20px">
-        <div style="background-image: url('<?php echo base_url('assets/img/packages/wildlife_dudhwaimage.jpg'); ?>'); height: 300px;background-position:center;background-size:cover;"> 
+        <div style="background-image: url('<?php echo base_url('assets/img/packages/wildlife_bardia.jpg'); ?>'); height: 300px;background-position:center;background-size:cover;"> 
         </div>
         <h1>
             <!--        //Put heading here-->
